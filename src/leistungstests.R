1a3e09786c774e9f305b86a9ecd81814dfba1d03
Dominik Rappaport <dominik@rappaport.at>
2024-10-03 12:45:22 +0200

    Wattvergleich hinzugefügt

--- src/leistungstests.R
+++ src/leistungstests.R
@@ -36,7 +36,7 @@ leistungstests.plot <- leistungstests %>%
   scale_y_continuous(labels = label_number(suffix = " W")) +
   scale_x_continuous(limits = c(10, 45), breaks = seq(10, 45, 5)) +
   bbc_style() +
-  labs(title = "Leistung im Verhältnis zur Sauerstoffaufnahme (mmol/l)")
+  labs(title = "Leistung im Verhältnis zur Sauerstoffaufnahme (ml/min/kg)", subtitle = "Messung vom 07. 11. 2023")
 
 finalise_plot(plot_name = leistungstests.plot,
               source = "Quelle: High Performance Coaching Clemens Rumpl, St. Pölten",
@@ -57,7 +57,7 @@ leistungstests.plot <- leistungstests %>%
   scale_y_continuous(labels = label_number(suffix = " W")) +
   scale_x_continuous(limits = c(0.3, 1), breaks = seq(0.3, 1, 0.1), labels = label_percent(suffix = " %")) +
   bbc_style() +
-  labs(title = "Leistung im Verhältnis zur Sauerstoffaufnahme (% VO2Max)")
+  labs(title = "Leistung im Verhältnis zur Sauerstoffaufnahme (% VO2Max)", subtitle = "Messung vom 07. 11. 2023")
 
 finalise_plot(plot_name = leistungstests.plot,
               source = "Quelle: High Performance Coaching Clemens Rumpl, St. Pölten",
@@ -119,3 +119,82 @@ leistungstests %>%
   scale_color_discrete(labels = c("VCO2 l/min", "VO2 l/min")) +
   theme_cowplot() +
   labs(title = "Gasaustausch während des VO2 Max Tests", x = "Zeit (in Minuten)", y = "l/min", colour = NULL)
+
+wattvergleich.cyclus2 <- read_csv("data/processed/wattvergleich/Cyclus 2.csv", col_types = cols_only("Type" = col_character(), "Message" = col_character(), "Value 1" = col_integer(), "Value 7" = col_integer())) %>%
+  mutate(Source = "Cyclus 2", Timestamp = `Value 1`, Watt = `Value 7`) %>%
+  select(Timestamp, Type, Message, Watt, Source) %>%
+  filter(Timestamp >= 1096705521, Type == "Data", Message == "record") %>%
+  mutate(Timestamp = as_datetime(Timestamp + 631065600, tz = "Europe/Vienna"))
+
+wattvergleich.favero <- read_csv("data/processed/wattvergleich/Favero Assioma Duo.csv", col_types = cols_only("Type" = col_character(), "Message" = col_character(), "Value 1" = col_integer(), "Value 4" = col_integer())) %>%
+  mutate(Source = "Favero Assioma Duo", Timestamp = `Value 1`, Watt = `Value 4`) %>%
+  select(Timestamp, Type, Message, Watt, Source) %>%
+  filter(Timestamp >= 1096705521, Type == "Data", Message == "record") %>%
+  mutate(Timestamp = as_datetime(Timestamp + 631065600, tz = "Europe/Vienna"))
+
+wattvergleich.laps <- read_csv("data/processed/wattvergleich/Laps.csv") %>%
+  mutate(Timestamp = as_datetime(Timestamp + 631065600, tz = "Europe/Vienna"))
+
+wattvergleich <- rbind(wattvergleich.favero, wattvergleich.cyclus2)
+
+wattvergleich %>%
+  mutate(Source = as.factor(Source)) %>%
+  mutate(Source = fct_relevel(Source, "Favero Assioma Duo", "Cyclus 2")) %>%
+  mutate(Timestamp = Timestamp - min(Timestamp)) %>%
+  ggplot(aes(x = Timestamp, y = Watt, colour = Source)) +
+  geom_line(linewidth = 1) +
+  geom_hline(yintercept = 0,
+             linewidth = 1,
+             colour = "#333333") +
+  scale_x_time(labels = label_time(format = "%H:%M")) +
+  scale_colour_manual(values = c("#FAAB18", "#1380A1")) +
+  scale_y_continuous() +
+  bbc_style() +
+  labs(title = "Wattvergleich")
+
+wattvergleich <- wattvergleich.cyclus2 %>% 
+  inner_join(wattvergleich.favero, by=join_by(Timestamp)) %>%
+  inner_join(wattvergleich.laps, by=join_by(Timestamp)) %>%
+  filter(Watt.x > 0) %>%
+  mutate(vergleich = Watt.y - Watt.x) %>%
+  group_by(Lap, Watt.x) %>%
+  summarise(vergleich.mittel = mean(vergleich)) %>%
+  mutate(Watt.Cyclus2 = Watt.x, Watt.FaveroAssioma = Watt.x + vergleich.mittel, Watt.abweichung = abs(vergleich.mittel)/Watt.x) %>%
+  select(Lap, Watt.Cyclus2, Watt.FaveroAssioma, Watt.abweichung)
+
+wattvergleich.diagram <- wattvergleich %>%
+  select(Lap, Watt.Cyclus2, Watt.FaveroAssioma) %>%
+  pivot_longer(cols = c(Watt.Cyclus2, Watt.FaveroAssioma), names_to = "Source", values_to = "Watt") %>%
+  ggplot(aes(x=Lap, y=Watt,colour=Source)) + 
+  geom_step(linewidth = 1) +
+  geom_hline(yintercept = 0,
+             linewidth = 1,
+             colour = "#333333") +
+  scale_colour_manual(values = c("#FAAB18", "#1380A1"), labels=c("Cyclus 2", "Favero Assioma Duo")) +
+  scale_x_continuous(limits=c(1,13), breaks=seq(1,13), labels=c("1.","2.","3.","4.","5.","6.","7.","8.","9.","10.","11.","12.","13. Runde")) +
+  scale_y_continuous(labels = label_number(suffix = " W"), limits=c(40, 240), breaks=seq(40, 240, 20)) +
+  bbc_style() +
+  theme(axis.text.x=element_text(hjust=0), plot.margin = margin(9,55,9,0)) +
+  labs(title = "Vergleich Wattmessung Cyclus 2 und Favero Assioma Duo", subtitle="Messung vom 01. 10. 2024")
+
+finalise_plot(plot_name = wattvergleich.diagram,
+              source = "Quelle: High Performance Coaching Clemens Rumpl, St. Pölten",
+              width_pixels = 1000,
+              save_filepath = "output/leistungstest2024_wattvergleich.jpg")
+
+wattvergleich.diagram <- wattvergleich %>%
+  ggplot(aes(x=Lap, y=Watt.abweichung)) +
+  geom_line(linewidth = 1, colour = "#1380A1") +
+  geom_hline(yintercept = 0,
+             linewidth = 1,
+             colour = "#333333") +
+  scale_x_continuous(limits=c(1,13), breaks=seq(1,13), labels=c("1.","2.","3.","4.","5.","6.","7.","8.","9.","10.","11.","12.","13. Runde")) +
+  scale_y_continuous(labels = label_percent()) +
+  bbc_style() +
+  theme(axis.text.x=element_text(hjust=0), plot.margin = margin(9,55,9,0)) +
+  labs(title = "Vergleich Wattmessung Cyclus 2 und Favero Assioma Duo (relativ)", subtitle="Messung vom 01. 10. 2024")
+
+finalise_plot(plot_name = wattvergleich.diagram,
+              source = "Quelle: High Performance Coaching Clemens Rumpl, St. Pölten",
+              width_pixels = 1000,
+              save_filepath = "output/leistungstest2024_wattvergleich_relativ.jpg")
