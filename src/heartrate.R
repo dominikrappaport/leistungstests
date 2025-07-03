6982b2073cb4e6c42388936b2f32ff66ef448c7a
Dominik Rappaport <dominik@rappaport.at>
2025-07-03 16:38:28 +0200

    Add HR and BB data for 2025 5-6

--- src/heartrate.R
+++ src/heartrate.R
@@ -32,6 +32,19 @@ hr.plot <- hr %>%
   theme(axis.text.x = element_text(hjust = 0),
         plot.margin = margin(9, 30, 9, 0))
 
+hr.histo.plot <- hr %>%
+  ggplot(aes(x = heartrate)) +
+  geom_histogram(aes(y = after_stat(count / sum(count))), binwidth = 5, colour = "white", fill = "#1380A1") +
+  geom_hline(yintercept = 0,
+             linewidth = 1,
+             colour = "#333333") +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(limits = c(50, 130), breaks = seq(50, 130, 5)) +
+  bbc_style() +
+  labs(title = "Herzfrequenz") +
+  theme(plot.margin = margin(9, 30, 9, 0))
+
+
 finalise_plot(
   plot_name = hr.plot,
   source = "Quelle: Messung mit Garmin Fenix",
